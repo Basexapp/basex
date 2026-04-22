8aade098b5a21c5d1ef638673bc997d700bc5bf8
Cortex IA <cortexautocom@gmail.com>
2026-04-21 21:19:04 -0300

    Alteração comum

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -23,7 +23,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   final supabase = Supabase.instance.client;
   String _statusMessage = 'Verificando atualizações...';
-  String _versaoExibida = '2.2.3';
+  String _versaoExibida = '2.2.4';
   Timer? _timer;
 
   @override
@@ -105,7 +105,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   String _getVersaoAtual() {
-    return '2.2.2';
+    return '2.2.4';
   }
 
   void _mostrarDialogAtualizacao() {
