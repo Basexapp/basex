4ccbf8a5f0f2fec1e4b42917eab1aa55b86889b1
Leonardo <cortexautocom@gmail.com>
2026-05-06 14:43:23 -0300

    Alteração comum

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -23,7 +23,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   final supabase = Supabase.instance.client;
   String _statusMessage = 'Verificando atualizações...';
-  String _versaoExibida = '2.2.12';
+  String _versaoExibida = '2.2.13';
   Timer? _timer;
 
   @override
@@ -105,7 +105,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   String _getVersaoAtual() {
-    return '2.2.12';
+    return '2.2.13';
   }
 
   void _mostrarDialogAtualizacao() {
